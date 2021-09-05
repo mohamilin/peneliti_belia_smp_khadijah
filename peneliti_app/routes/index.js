a2868b70a2195fcc8c1fdf091d021e0d40c6d7b5
Moh Amilin <amilinmoh99@gmail.com>
2021-09-05 10:24:23 +0700

    add : route

--- peneliti_app/routes/index.js
+++ peneliti_app/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/homepage', function(req, res, next) {
+  res.render('homepage', { title: 'Express' });
+});
+
 module.exports = router;
