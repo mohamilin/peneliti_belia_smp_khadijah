7ba3eed9b1b7840f96128056eaa57861bf79d514
Moh Amilin <amilinmoh99@gmail.com>
2021-09-05 12:22:32 +0700

    update

--- peneliti_app/routes/index.js
+++ peneliti_app/routes/index.js
@@ -10,4 +10,8 @@ router.get('/', function(req, res, next) {
   res.render('homepage', { title: 'Express' });
 });
 
+router.get('/kamar_mandi', function(req, res, next) {
+  res.render('kamar_mandi', { title: 'Express' });
+});
+
 module.exports = router;
